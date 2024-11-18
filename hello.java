598385b5d339e63a7b936964f058c029a511acf7
Badmus Abdullah <badmusakanbi@gmail.com>
2024-11-18 23:47:54 +0000

    first_sets

---
+++ hello.java
@@ -0,0 +1,12 @@
+public class hello {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 20;
+        
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is " + sum);
+        System.err.println("Badmus Abdullah");
+    }
+     
+}
