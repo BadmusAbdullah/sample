e9b2ed366af36f60465dc14a435b82810a3a3c77
Badmus Abdullah <badmusakanbi@gmail.com>
2024-11-19 22:27:53 +0000

    if statement

---
+++ ifStatement.java
@@ -0,0 +1,14 @@
+public class ifStatement {
+    public static void main(String[] args) {
+        int age = 75;
+        if (age >= 75) {
+            System.out.println("Ok Boomer!");
+        }
+        else if (age >= 18) {
+            System.out.println("You are an adult!");
+        }
+        else {
+            System.out.println("You are not yet an adult!");
+        }
+    }
+}
